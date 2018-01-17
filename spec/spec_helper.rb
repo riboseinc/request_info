bd8d6459d125cba7a69c5ed71e02a665b9258519
Sebastian Skałacki <skalee@gmail.com>
2018-01-17 11:20:56 +0100

    Rewrite spec helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,20 @@
-$LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
-require "request_info"
-require "pp"
+# (c) 2018 Ribose Inc.
+#
+#
+
+require "bundler"
+Bundler.require :default, :development
+
+Dir[File.expand_path "../support/**/*.rb", __FILE__].sort.each { |f| require f }
+
+RSpec.configure do |config|
+  # Enable flags like --only-failures and --next-failure
+  config.example_status_persistence_file_path = ".rspec_status"
+
+  # Disable RSpec exposing methods globally on `Module` and `main`
+  config.disable_monkey_patching!
+
+  config.expect_with :rspec do |c|
+    c.syntax = :expect
+  end
+end
