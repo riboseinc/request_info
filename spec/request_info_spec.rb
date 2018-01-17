0352330e485ab24f21acbda37a446680cc751213
Sebastian Skałacki <skalee@gmail.com>
2018-01-17 11:22:01 +0100

    Prove that configuration works
    Fix the only spec.

--- spec/request_info_spec.rb
+++ spec/request_info_spec.rb
@@ -1,7 +1,6 @@
 require 'spec_helper'
-require 'request_info'
 
-describe RequestInfo do
+RSpec.describe RequestInfo do
   it "has a version number" do
     expect(RequestInfo::VERSION).not_to be nil
   end
