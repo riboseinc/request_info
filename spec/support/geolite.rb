36caff3a51f16ac19bfc62baafbda75d7a751899
Sebastian Skałacki <skalee@gmail.com>
2018-01-23 15:11:17 +0100

    Remove support for ENV["GEOIPDBPATH"]
    Use RequestInfo::configure instead.

--- spec/support/geolite.rb
+++ spec/support/geolite.rb
@@ -1,2 +1,2 @@
-ENV["GEOIPDBPATH"] ||=
-  File.expand_path("../../../geolitedb/GeoLite2-City.mmdb", __FILE__)
+dbpath = File.expand_path("../../../geolitedb/GeoLite2-City.mmdb", __FILE__)
+RequestInfo.configure { |config| config.geoipdb_path = dbpath }
