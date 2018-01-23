fccbb7f5e4803dc7c651702e3bd496794902f92c
Sebastian Skałacki <skalee@gmail.com>
2018-01-23 13:11:50 +0100

    Stop using class variables in Configuration
    One step closer thread safety.

--- lib/request_info/configuration.rb
+++ lib/request_info/configuration.rb
@@ -1,38 +1,27 @@
 module RequestInfo
   class Configuration
-    # Path to the locale-name map csv file
-    def locale_name_map_path
-      @@locale_name_map_path ||= File.expand_path(
+    attr_accessor :locale_name_map_path, :locale_map_path, :geoip_path
+
+    def initialize
+      set_defaults
+    end
+
+    private
+
+    def set_defaults
+      self.locale_name_map_path = File.expand_path(
         "../../../data/locale_name_map.csv",
         __FILE__,
       )
-    end
 
-    def locale_name_map_path=(path)
-      @@locale_name_map_path = path.to_s
-    end
-
-    # Path to the country-locale map csv file
-    def locale_map_path
-      @@locale_map_path ||= File.expand_path(
+      self.locale_map_path = File.expand_path(
         "../../../data/country_locale_map.csv",
         __FILE__,
       )
-    end
 
-    def locale_map_path=(path)
-      @@locale_map_path = path.to_s
-    end
-
-    # Path to the GeoIPCity .dat file
-    def geoip_path
-      @@geoip_path ||=
+      self.geoip_path =
         ENV["GEOIPDBPATH"] ||
         "/usr/local/GeoIP/GeoIP2-City.mmdb"
-    end
-
-    def geoip_path=(path)
-      @@geoip_path = path.to_s
     end
   end
 end
