6e5ae6cb220d7b6aac9eb1f886740c9b1bf0291f
Sebastian Skałacki <skalee@gmail.com>
2018-03-03 21:20:46 +0100

    Rewrite preferred locale detection
    * Use Browser gem for analyzing Accept-Language header.
    * Simplify logic by using enumerators.
    * Document methods.

--- lib/request_info/detectors/locale_detector.rb
+++ lib/request_info/detectors/locale_detector.rb
@@ -1,24 +1,15 @@
 # (c) Copyright 2017 Ribose Inc.
 #
 
-require "request_info/locale"
 require "i18n"
+require "request_info/country_locale_map"
 
 module RequestInfo
   module Detectors
     module LocaleDetector
       def analyze(env)
         super
-
-        results = RequestInfo.results
-
-        # Find compatible locales
-        compat = RequestInfo::Locale.compatible_langs(
-          accept_language(env),
-        )
-
-        locale = compat.empty? ? default_locale : compat.first.first
-        results.locale = locale
+        RequestInfo.results.locale = detect_locale
       end
 
       def wrap_app
@@ -39,10 +30,40 @@ module RequestInfo
 
       private
 
-      # Extracts Accept-Language from env
+      def detect_locale
+        available_locales = ::I18n.available_locales.map(&:to_s)
+        user_preference.detect { |l| available_locales.include?(l) }
+      end
+
+      # Returns enumerator which yields locales which are preferred by user,
+      # starting with the best matching one.  The user preference is defined as
+      # concatenation of locales in Accept-Language HTTP header (already sorted
+      # according to respective weights), locales matching the user's location
+      # (guessed from the IP address), and finally the application's default
+      # locale.
       #
-      def accept_language(env)
-        env["HTTP_ACCEPT_LANGUAGE"]
+      # It is not guaranteed that these locales are available in I18n.
+      def user_preference
+        Enumerator.new do |y|
+          browser_locales.each { |l| y << l }
+          ip_locales.each { |l| y << l }
+          y << default_locale
+        end
+      end
+
+      # Locales preferred by user according to Accept-Language HTTP header.
+      def browser_locales
+        locales_arr = RequestInfo.results.browser.try(:accept_language) || []
+        locales_arr.flat_map { |l| [l.full, l.code] }
+      end
+
+      # Guessing of locales preferred by user basing on his location.
+      def ip_locales
+        ipinfo = RequestInfo.results.ipinfo || {}
+        country_code = ipinfo["country_code"]
+        return [] unless country_code
+        locales = CountryLocaleMap.instance.country_code_locales(country_code)
+        locales + locales.map { |l| l.split(/\W/, 2).first }
       end
 
       def default_locale
