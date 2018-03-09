49216d9e63c8188bb0c9e750433c355013aed91b
Sebastian Skałacki <skalee@gmail.com>
2018-03-09 13:29:16 +0100

    Fix indentation after recent changes
    The previous commit introduced nested style of class definitions.
    Subsequent indentation fix is required.

--- lib/request_info/detector_app.rb
+++ lib/request_info/detector_app.rb
@@ -7,36 +7,36 @@ require "request_info/detectors/timezone_detector"
 require "request_info/detectors/locale_detector"
 
 module RequestInfo
-# Rack middleware to process all specified detectors and sets results for the
-# current thread
-class DetectorApp
-  class << self
-    attr_accessor :detectors
-  end
-
-  attr_reader :analyzer, :app
-
-  def initialize(app)
-    @app = app
-
-    # TODO: make this list of detectors available for others to add/change
-    if !self.class.detectors
-      self.class.detectors = [
-        RequestInfo::Detectors::IpDetector,
-        RequestInfo::Detectors::BrowserDetector,
-        RequestInfo::Detectors::TimezoneDetector,
-        RequestInfo::Detectors::LocaleDetector,
-      ]
+  # Rack middleware to process all specified detectors and sets results for the
+  # current thread
+  class DetectorApp
+    class << self
+      attr_accessor :detectors
     end
 
-    @analyzer = ::RequestInfo::EnvAnalyzer.new(self.class.detectors)
-  end
+    attr_reader :analyzer, :app
 
-  def call(env)
-    analyzer.analyze(env)
-    analyzer.wrap_app do
-      app.call(env)
+    def initialize(app)
+      @app = app
+
+      # TODO: make this list of detectors available for others to add/change
+      if !self.class.detectors
+        self.class.detectors = [
+          RequestInfo::Detectors::IpDetector,
+          RequestInfo::Detectors::BrowserDetector,
+          RequestInfo::Detectors::TimezoneDetector,
+          RequestInfo::Detectors::LocaleDetector,
+        ]
+      end
+
+      @analyzer = ::RequestInfo::EnvAnalyzer.new(self.class.detectors)
+    end
+
+    def call(env)
+      analyzer.analyze(env)
+      analyzer.wrap_app do
+        app.call(env)
+      end
     end
   end
-end
 end
