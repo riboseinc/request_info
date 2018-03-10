863d6388c0575f8092c076e63d7cc40cc3a5a623
Sebastian Skałacki <skalee@gmail.com>
2018-03-10 08:09:44 +0100

    Test with unset GeoIP2 db path

--- spec/support/geolite.rb
+++ spec/support/geolite.rb
@@ -1,5 +1,7 @@
 # (c) Copyright 2017 Ribose Inc.
 #
 
-dbpath = File.expand_path("../../../geolitedb/GeoLite2-City.mmdb", __FILE__)
-RequestInfo.configure { |config| config.geoip2_db_path = dbpath }
+unless ENV.fetch("DISABLE_GEOIP2", false)
+  dbpath = File.expand_path("../../../geolitedb/GeoLite2-City.mmdb", __FILE__)
+  RequestInfo.configure { |config| config.geoip2_db_path = dbpath }
+end
