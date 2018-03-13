465e95d16d5bdf26bddc24dd01cb07aa95ebe501
Sebastian Skałacki <skalee@gmail.com>
2018-03-13 18:02:36 +0100

    Delete unused configuration options

--- spec/configuration_spec.rb
+++ spec/configuration_spec.rb
@@ -4,19 +4,12 @@
 require "spec_helper"
 
 RSpec.describe RequestInfo::Configuration do
-  it { is_expected.to respond_to(:locale_name_map_path) }
-  it { is_expected.to respond_to(:locale_name_map_path=) }
   it { is_expected.to respond_to(:locale_map_path) }
   it { is_expected.to respond_to(:locale_map_path=) }
   it { is_expected.to respond_to(:geoip2_db_path) }
   it { is_expected.to respond_to(:geoip2_db_path=) }
 
   describe "defaults" do
-    example do
-      val = subject.locale_name_map_path
-      expect(val).to point_to_existing_file & end_with(".csv")
-    end
-
     example do
       val = subject.locale_map_path
       expect(val).to point_to_existing_file & end_with(".csv")
