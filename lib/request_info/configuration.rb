0a85d02b7522f0cc2cf714768862ed021b4442e9
Peter Tam <peter.tam@ribose.com>
2018-08-14 11:50:55 +0800

    Revert "Delete unused configuration options"
    This reverts commit 465e95d16d5bdf26bddc24dd01cb07aa95ebe501.

--- lib/request_info/configuration.rb
+++ lib/request_info/configuration.rb
@@ -3,7 +3,7 @@
 
 module RequestInfo
   class Configuration
-    attr_accessor :geoip2_db_path
+    attr_accessor :locale_name_map_path, :locale_map_path, :geoip2_db_path
 
     def initialize
       set_defaults
@@ -12,7 +12,21 @@ module RequestInfo
     private
 
     def set_defaults
+      self.locale_name_map_path = default_locale_name_map_path
+      self.locale_map_path = default_locale_map_path
       self.geoip2_db_path = nil
+    end
+
+    def default_locale_name_map_path
+      File.expand_path("data/locale_name_map.csv", gem_root)
+    end
+
+    def default_locale_map_path
+      File.expand_path("data/country_locale_map.csv", gem_root)
+    end
+
+    def gem_root
+      File.expand_path("../../..", __FILE__)
     end
   end
 end
